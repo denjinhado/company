294c2bdeea1bd33fef389b847330fad1dc8ceef8
Kevin Arnista <denjinpunch@gmail.com>
2024-05-13 19:31:35 -0700

    hide navbar in mobile

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+const mobileMenu = document.querySelector(".mobile-menu");
 
+mobileMenu.addEventListener("click", navegacionResponsive);
+
+function navegacionResponsive() {
+  const navegacion = document.querySelector(".navbar");
+  if (navegacion.classList.contains("mostrar")) {
+    navegacion.classList.remove("mostrar");
+  } else {
+    navegacion.classList.add("mostrar");
+  }
+}
